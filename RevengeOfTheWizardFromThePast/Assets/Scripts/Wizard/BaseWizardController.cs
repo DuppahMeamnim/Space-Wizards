daf1143df8adccc2a3bb936dfbf4ed8e8c793441
eiTeyTrainers <97017576+eiTeyTrainers@users.noreply.github.com>
2024-11-26 22:16:00 +0200

    added animations by using a new animator organized and flipped player

---
+++ RevengeOfTheWizardFromThePast/Assets/Scripts/Wizard/BaseWizardController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class BaseWizardController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float walkSpeed = 20.0f;
+    [SerializeField] private float acceleration = 4000.0f;
+    [SerializeField] private float deceleration = 10000.0f;
+    [SerializeField] private string currentState;
+
+    //Animation States
+    const string WIZARD_IDLE = "Wizard_idle";
+    const string WIZARD_RUN = "Wizard_run";
+
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private WizardInputHandler inputHandler;
+
+    private void Awake()
+    {
+        inputHandler = GetComponent<WizardInputHandler>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        rb.gravityScale = 0;
+        rb.linearDamping = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 input = inputHandler.MoveInput;
+        Vector2 targetVelocity = input * walkSpeed;
+        
+        Vector2 velocityChange = targetVelocity - rb.linearVelocity;
+        float rate = input.magnitude > 0 ? acceleration : deceleration;
+        Vector2 movementForce = velocityChange * rate * Time.fixedDeltaTime;
+
+        rb.AddForce(movementForce, ForceMode2D.Force);
+        if(input.magnitude > 0)
+        {
+            ChangeAnimationState(WIZARD_RUN);
+        }
+        else
+        {
+            ChangeAnimationState(WIZARD_IDLE);
+        }
+
+        Vector3 originalScale = transform.localScale;
+
+        if (input.x > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+        else if (input.x < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+
+    }
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+
+        animator.Play(newState);
+
+        currentState = newState;
+    }
+}
+
